7c49160aae51169319a70140ba15df619cd47ed3
Ian Rash <redcodefinal@gmail.com>
2023-09-13 17:35:19 -0700

    Try again

--- rsrc/install.ps1
+++ rsrc/install.ps1
@@ -1,15 +1,12 @@
 git clone https://github.com/c-krit/ferox
-Copy-Item -Path "ferox/include" -Destination "rsrc/include" -Recurse -Force 
-Copy-Item -Path "ferox/src" -Destination "rsrc/src" -Recurse -Force 
-Remove-Item -Path "ferox" -Recurse -Force
 
-cl /c /Irsrc/include /Irsrc/src rsrc\ferox.c
-lib ferox.obj
+cd ferox
+nmake -f NMakefile
 
 $ErrorActionPreference= 'silentlycontinue'
-Remove-Item -Path "rsrc\include" -Recurse -Force
-Remove-Item -Path "rsrc\src" -Recurse -Force
-Remove-Item -Path "ferox.obj" -Force
+
 mkdir C:\ferox
-Copy-Item -Path "ferox.lib" -Destination "C:\ferox\ferox.lib" -Force 
-Move-Item -Path "ferox.lib" "rsrc\ferox.lib" -Force
+Copy-Item -Path "lib/ferox.lib" -Destination "C:\ferox\ferox.lib" -Force 
+Copy-Item -Path "lib/ferox.lib" -Destination "..\rsrc\ferox.lib" -Force 
+cd ..
+Remove-Item -Path "ferox" -Recurse -Force
